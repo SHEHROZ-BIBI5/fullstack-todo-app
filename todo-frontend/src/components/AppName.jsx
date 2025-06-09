85113a5a80d59dfd0bb27ae6344e5af3621f87c1
SHEHROZ-BIBI5 <shehrozbibi5@gmail.com>
2025-06-09 07:10:51 +0500

    Set up backend with Node.js and frontend with React.js for fullstack todo app 

---
+++ todo-frontend/src/components/AppName.jsx
@@ -0,0 +1,14 @@
+function AppName() {
+  return (
+    <>
+    <div className="text-center py-6 bg-white shadow">
+    <h1 className=" text-4xl font-bold   tracking-wide text-center mb-6 text-green-700">
+      🌟 TODO App
+    </h1>
+    <p className="text-gray-600 mt-2">Stay productive. Plan smart.</p>
+    </div>
+    </>
+  );
+}
+
+export default AppName;
---
+++ todo-frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+ function Footer() {
+  return (
+    <footer className="text-center py-4 bg-white shadow mt-10">
+      <p className="text-gray-600 font-medium">
+        🌸 Enjoy Your Day | Made with ❤️ by Shehroz
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
